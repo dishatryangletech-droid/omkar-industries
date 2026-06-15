c32e7ec06b49002d9027a0f105e1a406783a3e92
dishatryangletech-droid <disha.tryangletech@gmail.com>
2026-06-15 18:21:43 +0530

    remove extra files

--- check_user_img.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$user = \App\Models\User::first();
-echo "profile_image in DB: " . $user->profile_image;
